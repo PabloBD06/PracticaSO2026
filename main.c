f14fb177bd4dd83989c6f4e6f3039fd0df396f68
pabloshyguy <pablobea2006@gmail.com>
2026-09-18 16:19:30 +0200

    .

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 // Autor1: Pablo Bea Dopazo login: pablo.bea.dopazo
 // Autor2: Szymon Arthur Zieba Glaz login:
+// Autor1: Pablo Bea Dopazo login: pablo.bea.dopazo
+// Autor2: Szymon Arthur Zieba Glaz login:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MAXENTRADA 2048
 #define MAXENTRADA 2048
 #define MAXNOMBRE 1024
 
@@ -14,12 +17,24 @@ void MostrarDirActual() {
 
     if (getcwd(dir, MAXNOMBRE) == NULL)
         perror("Imposible obtener directorio");
+void MostrarDirActual() {
+    char dir[MAXNOMBRE];
+
+    if (getcwd(dir, MAXNOMBRE) == NULL)
+        perror("Imposible obtener directorio");
     else
+        printf("%s\n", dir);
         printf("%s\n", dir);
 }
 
 int ComprobarSegundoPlano(char *tr[]) {
+int ComprobarSegundoPlano(char *tr[]) {
     int i;
+    for (i = 0; tr[i] != NULL; i++)
+        if (!strcmp(tr[i], "&")) { /*& indica segundo plano*/
+            tr[i] = NULL;          /*es el ultimo argumento*/
+            return i;              /*si solo hay un & no se ejecuta nada en pplano*/
+        }
     for (i = 0; tr[i] != NULL; i++)
         if (!strcmp(tr[i], "&")) { /*& indica segundo plano*/
             tr[i] = NULL;          /*es el ultimo argumento*/
@@ -34,7 +49,17 @@ void Proceso(char *tr[], int splano) {
     int background = splano || ComprobarSegundoPlano(tr);
     if ((pid = fork()) == -1) {
         perror("Imposible crear proceso");
+void Proceso(char *tr[], int splano) {
+    pid_t pid;
+    void Cmd_exec(char **);
+    int background = splano || ComprobarSegundoPlano(tr);
+    if ((pid = fork()) == -1) {
+        perror("Imposible crear proceso");
         return;
+    }
+    if (pid == 0) { /*proceso hijo*/
+        Cmd_exec(tr);
+        exit(255); /*por si falla exec*/
     }
     if (pid == 0) { /*proceso hijo*/
         Cmd_exec(tr);
@@ -42,14 +67,24 @@ void Proceso(char *tr[], int splano) {
     }
     if (!background)
         waitpid(pid, NULL, 0);
+    if (!background)
+        waitpid(pid, NULL, 0);
 }
 
 /*********************************************/
 /*************COMANDOS DEL SHELL************************/
 
-void Cmd_autores() {
-    printf("Los autores del shell....\n");
+void Cmd_autores(char *tr) {
+     if (!tr[0])
+        printf("Pablo Bea Dopazo(pablo.bea.dopazo)\nSzymon Artur Zieba Glaz(szymon.zieba)\n");
+    else if (strcmp(tr, "-n"))
+        printf("Pablo Bea Dopazo\nSzymon Artur Zieba Glaz\n");
+    else if (strcmp(tr, "-l"))
+        printf("pablo.bea.dopazo\nszymon.zieba\n");
+    else
+        printf("opcion no existente");
 }
+
 
 void Cmd_exec(char *tr[]) {
     if (execvp(tr[0], tr) == -1)
@@ -58,7 +93,11 @@ void Cmd_exec(char *tr[]) {
 
 void Cmd_splano(char *arg[]) {
     Proceso(arg, 1);
+void Cmd_splano(char *arg[]) {
+    Proceso(arg, 1);
 }
+void Cmd_pplano(char *arg[]) {
+    Proceso(arg, 0);
 void Cmd_pplano(char *arg[]) {
     Proceso(arg, 0);
 }
@@ -68,12 +107,26 @@ void Cmd_chdir(char *dir) {
         MostrarDirActual();
     else if (chdir(dir) == -1)
         perror("Imposible cambiar directorio");
+void Cmd_chdir(char *dir) {
+    if (dir == NULL)
+        MostrarDirActual();
+    else if (chdir(dir) == -1)
+        perror("Imposible cambiar directorio");
 }
 
 void Cmd_pwd() {
+void Cmd_pwd() {
     MostrarDirActual();
 }
+void Cmd_date(){
 
+}
+
+void Cmd_pid(char *arg) {
+    if (arg == NULL)
+        printf("El pid del proceso es %d\n", (int)getpid());
+    else if (!strcmp(arg, "-p"))
+        printf("El pid del proceso padre es %d\n", (int)getppid());
 void Cmd_pid(char *arg) {
     if (arg == NULL)
         printf("El pid del proceso es %d\n", (int)getpid());
@@ -85,14 +138,12 @@ void Cmd_pid(char *arg) {
 void DecidirComando(char *tr[]) {
     if (tr[0] == NULL) /*por si cambiamos lo de TroearCadena==0*/
         return;        /*no hace falta que ya comprobamos que TrocearCadena no devielve 0*/
-    if (!strcmp(tr[0], "fin"))
+    if (!strcmp(tr[0], "bye") || !strcmp(tr[0], "quit") || !strcmp(tr[0], "exit"))
         exit(0);
-    else if (!strcmp(tr[0], "quit"))
-        exit(0);
-    else if (!strcmp(tr[0], "exit"))
-        exit(0);
+    else if (!strcmp(tr[0], "date"))
+        Cmd_date();
     else if (!strcmp(tr[0], "autores"))
-        Cmd_autores();
+        Cmd_autores(tr[1]);
     else if (!strcmp(tr[0], "exec"))
         Cmd_exec(tr + 1);
     else if (!strcmp(tr[0], "pplano"))
@@ -122,11 +173,31 @@ void ProcesarEntrada(char *entrada) {
     if (TrocearCadena(entrada, tr) == 0) /*no hay nada*/
         return;
     DecidirComando(tr);
+int TrocearCadena(char *cadena, char *trozos[]) {
+    int i = 1;
+    if ((trozos[0] = strtok(cadena, " \n\t")) == NULL)
+        return 0;
+    while ((trozos[i] = strtok(NULL, " \n\t")) != NULL)
+        i++;
+    return i;
+}
+void ProcesarEntrada(char *entrada) {
+    char *tr[MAXENTRADA / 2];
+    if (TrocearCadena(entrada, tr) == 0) /*no hay nada*/
+        return;
+    DecidirComando(tr);
 }
 
 int main(int argc, char *argv[], char *ent[]) {
     char entrada[MAXENTRADA];
+int main(int argc, char *argv[], char *ent[]) {
+    char entrada[MAXENTRADA];
 
+    while (1) {
+        printf("-> ");
+        fgets(entrada, MAXENTRADA, stdin);
+        ProcesarEntrada(entrada);
+    }
     while (1) {
         printf("-> ");
         fgets(entrada, MAXENTRADA, stdin);
