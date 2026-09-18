07e6272cae48c7f7998f4b3814951599f173351f
Madra_Ropucha <szymon291203@gmail.com>
2026-09-18 17:21:34 +0200

    .

--- main.c
+++ main.c
@@ -7,10 +7,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
-<<<<<<< HEAD
-=======
-#include <sys/utsname.h>
->>>>>>> refs/remotes/origin/main
 
 
 #define MAXENTRADA  2048
@@ -67,7 +63,6 @@ void Cmd_autores(char *tr) {
         printf("pablo.bea.dopazo\nszymon.zieba\n");
 }
 
-<<<<<<< HEAD
 void Cmd_date(char *tr){
     time_t now = time(NULL);
     struct tm *t  = localtime(&now);
@@ -87,56 +82,8 @@ void Cmd_date(char *tr){
         strftime(hour, sizeof(hour), "%H:%M:%S", t);
         printf("%s\n", hour);
     }    
-=======
-void Cmd_sysinfo(){
-    struct utsname info;
-
-    if (uname(&info) == -1) {
-        perror("uname");
-        return;
-    }
-
-    printf("Sistema operativo: %s\n", info.sysname);
-    printf("Nombre de la máquina: %s\n", info.nodename);
-    printf("Release/kernel: %s\n", info.release);
-    printf("Versión del kernel: %s\n", info.version);
-    printf("Arquitectura: %s\n", info.machine);
->>>>>>> refs/remotes/origin/main
 }
 
-void Cmd_help(char *tr){
-    if(!tr){
-        printf("Cmds practica 0: prompt numserie limits systemd command historic\n" 
-                "Cmds practica 1: exit bye date pid authors sysinfo help chdir open close listopen dup lseek readstr writestr makefile makedir delete deltree listfile list\n"
-                "Cmds practica 2: allocate deallocate i-o read write memory recurse memfill memdump\n"
-                "Cmds practica 3: getuid setuid prio path showenv showvar changevar changevarWrong fork ejec ejecpri pplano pplanopri splano splanopri jobs deljobs\n");
-    }
-    else if (strcmp(tr, "exit") || strcmp(tr, "exit"))
-        printf("Termina la ejecucion del shell");
-    else if (strcmp(tr, "date"))
-        printf("date: [-d|-t]	Muestra la fecha y/o la hora actual");
-    else if (strcmp(tr, "pid"))
-        printf("pid: [-p]	Muestra el pid del shell o de su proceso padre");
-    else if (strcmp(tr, "authors"))
-        printf("authors: [-n|-l]	Muestra los nombres y/o logins de los autores");
-    else if (strcmp(tr, "sysinfo"))
-        printf("sysinfo: 	Muestra informacion de la maquina donde corre el shell");
-    else if (strcmp(tr, "help"))
-        printf("help: [cmd|-lt|-T|-all]	Muestra ayuda sobre los comandos"
-	            "cmd: info sobre el comando cmd");
-    else if (strcmp(tr, "chdir"))
-        printf("chdir: [dir]	Cambia (o muestra) el directorio actual del shell");
-    else if (strcmp(tr, "open"))
-        printf("open: fich m1 m2...	Abre el fichero fich"
-	            "y lo anade a la lista de ficheros abiertos del shell"
-	            "m1, m2..es el modo de apertura (or bit a bit de los siguientes)"
-	            "cr: O_CREAT	ap: O_APPEND"
-	            "ex: O_EXCL 	ro: O_RDONLY"
-	            "rw: O_RDWR 	wo: O_WRONLY"
-	            "tr: O_TRUNC");
-    else
-        printf("faltan comandos");
-                }
 void Cmd_exec (char *tr[])
 {
   if (execvp(tr[0],tr)==-1)
@@ -180,17 +127,10 @@ void DecidirComando(char *tr[])
   if (tr[0]==NULL)  /*por si cambiamos lo de TroearCadena==0*/
     return;         /*no hace falta que ya comprobamos que TrocearCadena no devielve 0*/
   if (!strcmp(tr[0],"quit") || !strcmp(tr[0],"exit")) exit(0);
-<<<<<<< HEAD
   else if (!strcmp(tr[0],"date")) Cmd_date(tr[1]);
   else if (!strcmp(tr[0],"pid")) Cmd_pid (tr[1]);
   else if (!strcmp(tr[0],"authors")) Cmd_autores(tr[1]);
   else if (!strcmp(tr[0],"sysinfo")) ;
-=======
-  else if (!strcmp(tr[0],"date")) Cmd_autores(tr[1]);
-  else if (!strcmp(tr[0],"pid")) Cmd_pid (tr[1]);
-  else if (!strcmp(tr[0],"authors")) Cmd_autores(tr[1]);
-  else if (!strcmp(tr[0],"sysinfo")) Cmd_sysinfo();
->>>>>>> refs/remotes/origin/main
   else if (!strcmp(tr[0],"help")) ;
   else if (!strcmp(tr[0],"chdir")) ;
   else if (!strcmp(tr[0],"open")) ;
