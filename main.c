fc687f3831e9944ad323493336fcabd25563c7ee
pabloshyguy <pablobea2006@gmail.com>
2026-09-18 12:44:47 +0200

    Primer commit

---
+++ main.c
@@ -0,0 +1,17 @@
+bool terminado= false;
+void imprimirPrompt(){
+    printf("&");
+}
+void leerEntrada(){
+
+}
+void procesarEntrada(){
+    
+}
+int main(){
+    while (!terminado){ 
+        imprimirPrompt();
+        leerEntrada();
+        procesarEntrada();
+    }
+}
